3282343a552a972037a5ec6afd9ab058fd4e39ed
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2022-08-10 00:35:18 +0200

    cr fix

--- crExtractor.py
+++ crExtractor.py
@@ -6,18 +6,22 @@ import random
 import sys
 
 headings = camelot.read_pdf(str(sys.argv[1]), multiple_tables=True, columns=['100,150,200,250,300,350,400,450,500,550,600,650,700,750,800'], pages=sys.argv[2], flavor="stream", edge_tol=10, row_tol=16)
+if headings.n > 0:
+    headings[0].parsing_report
+    headings[0].to_json('./jsons/head.json')
+    result = []
+    with open('./jsons/head.json') as json_file:
+        try:
+            result.append(json.load(json_file))
+        except ValueError:
+            print(f)
 
-headings[0].parsing_report
-headings[0].to_json('./jsons/head.json')
-result = []
-with open('./jsons/head.json') as json_file:
-    try:
-        result.append(json.load(json_file))
-    except ValueError:
-        print(f)
+    with open("merged_file.json", "w") as outfile:
+        json.dump(result, outfile)
 
-with open("merged_file.json", "w") as outfile:
-    json.dump(result, outfile)
-
-jsonData = json.dumps(result)
-print(jsonData)
+    json_data = json.dumps(result)
+    print(json_data)
+else:
+    data = []
+    json_data = json.dumps(data)
+    print(json_data)
