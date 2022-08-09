d53e31e083bbfe0187f2a66a1e7df3155930eb9f
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2022-08-10 01:29:51 +0200

    fix

--- crExtractor.py
+++ crExtractor.py
@@ -8,15 +8,15 @@ import sys
 headings = camelot.read_pdf(str(sys.argv[1]), multiple_tables=True, columns=['100,150,200,250,300,350,400,450,500,550,600,650,700,750,800'], pages=sys.argv[2], flavor="stream", edge_tol=10, row_tol=16)
 if headings.n > 0:
     headings[0].parsing_report
-    headings[0].to_json('./jsons/head.json')
+    headings[0].to_json('/var/www/html/staging/jsons/head.json')
     result = []
-    with open('./jsons/head.json') as json_file:
+    with open('/var/www/html/staging/jsons/head.json') as json_file:
         try:
             result.append(json.load(json_file))
         except ValueError:
             print(f)
 
-    with open("merged_file.json", "w") as outfile:
+    with open("/var/www/html/staging/merged_file.json", "w") as outfile:
         json.dump(result, outfile)
 
     json_data = json.dumps(result)
