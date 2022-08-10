a2b28d0d947f101a54feaf7f9d35ea349ffd3207
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2022-08-11 01:56:44 +0200

    fix pages

--- app/Http/Controllers/CentraleRischiController.php
+++ app/Http/Controllers/CentraleRischiController.php
@@ -769,6 +769,7 @@ class CentraleRischiController extends Controller
         }
 
         $totalPages = $processGetPages->getOutput();
+        dd($totalPages);
         if($totalPages > 0 && is_int((int)$totalPages)) {
             for ($pageToExtract = 1; $pageToExtract <= $totalPages; $pageToExtract++) {
                 $liveStatus = round( (($pageToExtract / $totalPages) * 100), 1 ). "% processato";
