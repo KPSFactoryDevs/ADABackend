e2e64c17bfd2fa3d86b2b7ee53d4efdd3e3e1cb3
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2022-08-17 17:10:49 +0200

    fix

--- app/Http/Controllers/CentraleRischiController.php
+++ app/Http/Controllers/CentraleRischiController.php
@@ -200,11 +200,11 @@ class CentraleRischiController extends Controller
 
         $base64CentraleRischi = $request->base64;
 
-        $storedFile = Storage::disk('public')->putFileAs('', $base64CentraleRischi, time().'_'.$base64CentraleRischi->getClientOriginalName());
+        $storedFile = Storage::disk('public')->putFile('', $base64CentraleRischi);
         $storeFullPath = asset('centraleRischi') . '/' . $storedFile;
 
         $newDocumentData = [
-            'filename' => $storedFile,
+            'filename' => $base64CentraleRischi, time().'_'.$base64CentraleRischi->getClientOriginalName(),
             'path' => $storeFullPath,
             'type' => 'centrale rischi',
             'codice_documento' => rand(1, 999999999),
