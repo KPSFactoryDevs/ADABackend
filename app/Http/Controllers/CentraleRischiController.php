2b9c5a3f40c97892c47d2142a48b7271a483071e
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2022-08-24 23:55:34 +0200

    fix

--- app/Http/Controllers/CentraleRischiController.php
+++ app/Http/Controllers/CentraleRischiController.php
@@ -291,8 +291,8 @@ class CentraleRischiController extends Controller
 
 
             //
-            $totaleAffidamentiTable = $crHelper->getTotaleAffidamenti($categories, $latestYear, $latestMonth, $banks);
-            $totaleAffidamentiGeneral = $crHelper->getTotaleAffidamentiGeneral($categories, $latestYear, $latestMonth, $banks);
+         //   $totaleAffidamentiTable = $crHelper->getTotaleAffidamenti($categories, $latestYear, $latestMonth, $banks);
+         //   $totaleAffidamentiGeneral = $crHelper->getTotaleAffidamentiGeneral($categories, $latestYear, $latestMonth, $banks);
             $totAffidamentiConPesiPerBanca = $crHelper->getPesiAffidamentiPerBanca($categories, $latestYear, $latestMonth, $banks);
             $scoreCR = $crHelper->getScoring($banks);
 
