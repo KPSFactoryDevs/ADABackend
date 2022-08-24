4812462a2244a455f6afed7a3cfd7c0186ad59df
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2022-08-25 00:29:52 +0200

    fix

--- app/Http/Controllers/CentraleRischiController.php
+++ app/Http/Controllers/CentraleRischiController.php
@@ -246,7 +246,7 @@ class CentraleRischiController extends Controller
 
                 if(!isset($crAndamentaleData['newDates'])) {
                     $lastDate = new DateTime(cr::select('date')->where('document_id', $crAndamentaleData['period'])->orderBy('date', 'desc')->first()->date);
-                    $earlierDate = new DateTime(cr::select('date')->where('document_id', $crAndamentaleData['period'])->orderBy('date', 'desc')->first()->date)->modify('-23 months');
+                    $earlierDate = new DateTime(cr::select('date')->where('document_id', $crAndamentaleData['period'])->orderBy('date', 'desc')->first()->date->modify('-23 months'));
                 } else {
                     $earlierDate = $crAndamentaleData['newDates']['data_inizio'];
                     $lastDate = $crAndamentaleData['newDates']['data_fine'];
