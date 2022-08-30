e92c2235f9f8c8ba52711fc1faf58426fedbfb14
CristianLoVerso <cri.loverso@gmail.com>
2022-08-30 15:35:03 +0200

    pdf controller

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -37,9 +37,11 @@ class PDFController extends Controller
         // $idBilancio = $id;
 
         if (DB::table('basic')->where('bilancio_id', '=', $idBilancio)->count() == 0) {
-            dd(DB::table('basic')->where('bilancio_id', '=', $idBilancio)->get());
             $msg = "Non è stata salvata alcuna analisi per questo bilancio";
-            return view('allerta.empty', compact(['msg']));
+            return response()->json([
+				'error' => false,
+				'Message' => 'Non è stata salvata alcuna analisi per questo bilancio'
+			]);
         }   
 
         $voci = DB::table('vocis')->get();
