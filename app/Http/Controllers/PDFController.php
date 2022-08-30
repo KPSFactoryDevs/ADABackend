b3b6e720742dcbabd91ed205ba5c0bf3a58371e2
CristianLoVerso <cri.loverso@gmail.com>
2022-08-30 15:41:24 +0200

    fix cr e allerta

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -34,10 +34,8 @@ class PDFController extends Controller
         $jsonData['currentYear'] = $attributes['current_year'];
         $jsonData['prevYear'] = $attributes['prev_year'];
         $tipoAzienda = $attributes['tipo_azienda'];
-        // $idBilancio = $id;
 
         if (DB::table('basic')->where('bilancio_id', '=', $idBilancio)->count() == 0) {
-            $msg = "Non è stata salvata alcuna analisi per questo bilancio";
             return response()->json([
 				'error' => false,
 				'Message' => 'Non è stata salvata alcuna analisi per questo bilancio'
