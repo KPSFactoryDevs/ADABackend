0afa9890bfb5a7e3cf9b308062ff47bf9bdf1bd4
CristianLoVerso <cri.loverso@gmail.com>
2022-09-06 15:33:41 +0200

    cercando di fixare reportBasicPDF

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,25 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
+      /*  return $next($request)->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, CurrentCompany')
-        ->header('Access-Control-Expose-Headers', 'Content-Range, X-Total-Count');
+        ->header('Access-Control-Expose-Headers', 'Content-Range, X-Total-Count');  */
+
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, CurrentCompany',
+            'Access-Control-Expose-Headers' => 'Content-Range, X-Total-Count'
+        ];
+
+        $response = $next($request);
+
+        foreach($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+
+        return $response;
+
     }
 }
