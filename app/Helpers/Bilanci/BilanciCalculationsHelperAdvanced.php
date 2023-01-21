3881723506a252511583c64e2d6a3199610d710d
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2023-01-21 15:07:25 +0100

    fix indexes

--- app/Helpers/Bilanci/BilanciCalculationsHelperAdvanced.php
+++ app/Helpers/Bilanci/BilanciCalculationsHelperAdvanced.php
@@ -34,9 +34,7 @@ class BilanciCalculationsHelperAdvanced
             ->where('period', $period)
             ->where('voiceFullName', $elementName)
             ->get();
-if($elementName = "CostiProduzioneAccantonamentiRischi") {
-    echo ($elementoFromQuery);
-}
+
         if (isset($elements[$elementName])) {
             if ($period == 1) {
                 $value = array_first($elements[$elementName])['value'];
@@ -46,9 +44,6 @@ if($elementName = "CostiProduzioneAccantonamentiRischi") {
 
             return $value;
         } elseif (count($elementoFromQuery) > 0) {
-            if($elementName = "CostiProduzioneAccantonamentiRischi") {
-                echo ($elementoFromQuery->first()->value);
-            }
             return $elementoFromQuery->first()->value;
         } else {
             if (!isset($this->_missingVoicesArray[$indexName])) {
