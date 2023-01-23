be0fce3c196c7148fe5a5219f5ca482f9f804893
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2023-01-23 22:53:38 +0100

    test fix

--- app/Helpers/Bilanci/BilanciHelper.php
+++ app/Helpers/Bilanci/BilanciHelper.php
@@ -143,7 +143,7 @@ class BilanciHelper
 
         //  $arraySoglie[$label] = range::where([['range_min', '<', $arrayIndici[$label]], ['range_max', '>', $arrayIndici[$label]], ['indice', '=', $label], ['tipo_azienda', '=', $tipoAzienda]])->with('pesi')->get();
             $arraySoglie[$label] = range::where(
-                [['range_min', '>', $arrayIndici[$label]], ['range_max', '<', $arrayIndici[$label]], ['indice', '=', $label], ['tipo_azienda', '=', 'Generica']]
+                [['range_min', '<', $arrayIndici[$label]], ['range_max', '>', $arrayIndici[$label]], ['indice', '=', $label], ['tipo_azienda', '=', 'Generica']]
             )->with('pesi')
             ->get();
 
