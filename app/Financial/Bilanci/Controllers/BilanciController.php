84f97f8daedda41250dd6d67de2c4d06c0ad87db
“cristian” <c.loverso@kpsfactory.com>
2023-01-25 18:21:39 +0100

    fix analisis api

--- app/Financial/Bilanci/Controllers/BilanciController.php
+++ app/Financial/Bilanci/Controllers/BilanciController.php
@@ -106,7 +106,7 @@ class BilanciController extends Controller
                     'idDocumento' => $document->id,
                     'renderHTML' => $renderHTML,
                     'bilancioJSON' => $bilancioJSON,
-                    'bilancioAnalisi' => $bilanciHelper->getIndexesForBalanceTaxonomy($document->id, $filePath, $readXBRL),
+                    'bilancioAnalisi' => $bilanciHelper->getIndexesForBalanceTaxonomy($document->id, $filePath, $readXBRL, $document->codice_documento),
                 ], 200);
             } else {
                 return response()->json([
