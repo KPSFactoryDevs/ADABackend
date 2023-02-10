82e2fc4c22dc9d4e9320bc6e82b73349d11d0457
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2023-02-10 19:36:48 +0100

    pdf allerta

--- resources/views/frontend/reportBasic.blade.php
+++ resources/views/frontend/reportBasic.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
-
+<title>Report Analisi Bilancio {{ $datiImpresa['ragione_sociale'] }}</title>
     </head>
     <body>
 
