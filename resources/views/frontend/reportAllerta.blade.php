7f950fb4210feace576d3192a697d6ec3b86fc3b
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2023-02-11 18:25:17 +0100

    fix score string

--- resources/views/frontend/reportAllerta.blade.php
+++ resources/views/frontend/reportAllerta.blade.php
@@ -310,7 +310,275 @@
 
     <table>
         <tr>
-            <th class="heading" colspan="10" >Valutazione Negativa Dell'analisi Sintetica CR</th>
+            <th class="heading" colspan="10">Valutazione Negativa Dell'analisi Sintetica CR</th>
+            <th colspan="2"><b>Esito</b></th>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti fatture e avvisi di pagamento di cui non si è rispettata la scadenza?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Ci sono ritardi nei pagamenti ai fornitori superiori a 90 giorni per un ammontare superiore a quello dei debiti non scaduti?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">I fornitori hanno modificato le condizioni di pagamento delle forniture (ad es. pagamento dilazionato a pagamento anticipato)?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Avete inoltrato richieste di rimodulazione delle scadenze nei pagamenti richiedendo di aumentare la dilazione?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa"  colspan="10">Sono state modificate le condizioni di incasso da parte dei clienti con un conseguente allungamento dei tempi di incasso?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono stati registrati mancati incassi per importi considerevoli superiori a 90gg?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti contenziosi in atto con clienti o fornitori?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti criticità con clienti derivanti da non conformità o ritardi?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+    </table>
+
+
+
+
+
+
+
+
+
+
+    <h5> Anomalie Dei Pagamenti Verso Controparti Commerciali</h5>
+
+
+    <table>
+        <tr>
+            <th class="heading" colspan="10">Valutazione Negativa Dell'analisi Sintetica CR</th>
+            <th colspan="2"><b>Esito</b></th>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti fatture e avvisi di pagamento di cui non si è rispettata la scadenza?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Ci sono ritardi nei pagamenti ai fornitori superiori a 90 giorni per un ammontare superiore a quello dei debiti non scaduti?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">I fornitori hanno modificato le condizioni di pagamento delle forniture (ad es. pagamento dilazionato a pagamento anticipato)?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Avete inoltrato richieste di rimodulazione delle scadenze nei pagamenti richiedendo di aumentare la dilazione?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa"  colspan="10">Sono state modificate le condizioni di incasso da parte dei clienti con un conseguente allungamento dei tempi di incasso?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono stati registrati mancati incassi per importi considerevoli superiori a 90gg?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti contenziosi in atto con clienti o fornitori?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti criticità con clienti derivanti da non conformità o ritardi?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+    </table>
+
+
+
+
+
+
+
+
+    <h5> Anomalie Dei Pagamenti Verso Controparti Commerciali</h5>
+
+
+    <table>
+        <tr>
+            <th class="heading" colspan="10">Valutazione Negativa Dell'analisi Sintetica CR</th>
+            <th colspan="2"><b>Esito</b></th>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti fatture e avvisi di pagamento di cui non si è rispettata la scadenza?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Ci sono ritardi nei pagamenti ai fornitori superiori a 90 giorni per un ammontare superiore a quello dei debiti non scaduti?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">I fornitori hanno modificato le condizioni di pagamento delle forniture (ad es. pagamento dilazionato a pagamento anticipato)?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Avete inoltrato richieste di rimodulazione delle scadenze nei pagamenti richiedendo di aumentare la dilazione?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa"  colspan="10">Sono state modificate le condizioni di incasso da parte dei clienti con un conseguente allungamento dei tempi di incasso?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono stati registrati mancati incassi per importi considerevoli superiori a 90gg?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti contenziosi in atto con clienti o fornitori?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+
+        <tr>
+            <td class="dati_impresa" colspan="10">Sono presenti criticità con clienti derivanti da non conformità o ritardi?</td>
+            <td class="dati_impresa" colspan="2">No</td>
+        </tr>
+        <tr>
+            <td class="dati_impresa" colspan="12" style="background:lightgrey;">Spiegazioni: <b>N/A</b></td>
+        </tr>
+
+    </table>
+
+
+
+
+
+
+
+    <h5> Anomalie Dei Pagamenti Verso Controparti Commerciali</h5>
+
+
+    <table>
+        <tr>
+            <th class="heading" colspan="10">Valutazione Negativa Dell'analisi Sintetica CR</th>
             <th colspan="2"><b>Esito</b></th>
         </tr>
         <tr>
