9e743325e418d77c3951ba290a05d3126868fdaf
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2023-02-17 00:33:10 +0100

    return user data in login

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -30,7 +30,7 @@ class ApiAuthController extends Controller
 
             if (Hash::check($request->password, $user->password)) {
                 $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['token' => $token];
+                $response = ['token' => $token, 'user' => $user];
                 return response($response, 200);
             } else {
                 $response = ["message" => "La password non è corretta"];
