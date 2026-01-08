1b410d206e4ca63435793ba04233d1c2fe53f42f
dewynn <cpngwy@gmail.com>
2026-01-08 11:38:34 +0000

    Push branch stage

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Database Connections
+    | Database Connections - use mysql
     |--------------------------------------------------------------------------
     |
     | Here are each of the database connections setup for your application.
@@ -29,7 +29,7 @@ return [
     |
     | All database work in Laravel is done through the PHP PDO facilities
     | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
+    | choice installed on your machine before you begin development.	
     |
     */
 
