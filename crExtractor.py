fbdcbd81d2fab0ed5d099a6e721aadbe66161189
Federico Megna <federicomegna@MacBook-Pro-di-Federico-2.local>
2026-01-29 21:55:46 +0100

    fix path

--- crExtractor.py
+++ crExtractor.py
@@ -5,12 +5,12 @@ import os
 import random
 import sys
 
-headings = camelot.read_pdf(str(sys.argv[1]), multiple_tables=True, columns=['100,150,200,250,300,350,400,450,500,550,600,650,700,750,800'], pages=sys.argv[2], flavor="stream", edge_tol=10, row_tol=16)
+headings = camelot.read_pdf(str(sys.argv[1]), multiple_tables=True, columns=['100,150,200,250,300,350,400,450,500,550,600,650,700,750,800'], pages=sys.argv[2], flavor="stream", edge_tol=10, row_tol=20)
 if headings.n > 0:
     headings[0].parsing_report
-    headings[0].to_json('/var/www/html/staging/jsons/head.json')
+    headings[0].to_json('/var/www/html/ADABackend/jsons/head.json')
     result = []
-    with open('/var/www/html/staging/jsons/head.json') as json_file:
+    with open('/var/www/html/ADABackend/jsons/head.json') as json_file:
         try:
             result.append(json.load(json_file))
         except ValueError:
